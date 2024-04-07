ddedf8b1326c074607795eaa88042212aa13b0c9
SaranRio <saranrio21@gmail.com>
2024-04-07 14:31:23 +0530

    first commit

---
+++ src/main/java/com/crud/fullstackbackend/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.crud.fullstackbackend.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(Long id){
+        super("Could not found the user with the id "+ id);
+    }
+
+}
---
+++ src/main/java/com/crud/fullstackbackend/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.crud.fullstackbackend.repository;
+import com.crud.fullstackbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface UserRepository  extends JpaRepository<User,Long>
+{
+}
